5b9a809332ae818e2ac4edbebbd65013b81002c6
mhd-faizzan <muhammadfaizzan@outlook.com>
2026-05-12 20:15:04 +0200

    update ProfileReport remove scores add activity

--- frontend/src/components/ProfileReport.jsx
+++ frontend/src/components/ProfileReport.jsx
@@ -1,6 +1,5 @@
 import ProfileHeader from './ProfileHeader'
-import ScoreCards from './ScoreCards'
-import HireScore from './HireScore'
+import ActivitySummary from './ActivitySummary'
 import TechStack from './TechStack'
 import TopRepos from './TopRepos'
 import Checklist from './Checklist'
@@ -9,8 +8,7 @@ export default function ProfileReport({ report }) {
   return (
     <div>
       <ProfileHeader report={report} />
-      <ScoreCards scores={report.scores} />
-      <HireScore score={report.scores.hire} />
+      <ActivitySummary activity={report.activity} />
       <TechStack languages={report.languages} />
       <TopRepos repos={report.top_repos} />
       <Checklist checklist={report.checklist} />
